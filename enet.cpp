abfc56989b1be2cc4824d3ec84674135761f3e8e
Modiz <77518855+ModizX@users.noreply.github.com>
2021-04-12 23:00:08 +0100

    Add files via upload

--- enet.cpp
+++ enet.cpp
@@ -1096,8 +1096,25 @@ enum BlockTypes {
 	UNKNOWN
 };
 
+#define Property_Zero 0
+#define Property_NoSeed 1
+#define Property_Dropless 2
+#define Property_Beta 4
+#define Property_Mod 8
+#define Property_Chemical 12
+#define Property_Untradable 16
+#define Property_Wrenchable 32
+#define Property_MultiFacing 64
+#define Property_Permanent 128
+#define Property_AutoPickup 256
+#define Property_WorldLock 512
+#define Property_NoSelf 1024
+#define Property_RandomGrow 2048
+#define Property_Public 4096
+#define Property_Foreground 8192
 
-struct ItemDefinition {
+struct ItemDefinition
+{
 	int id;
 
 	unsigned char editableType = 0;
